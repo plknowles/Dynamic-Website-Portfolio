005e8be9890942627b2a409899f9f22ea3fb25a6
Peter Knowles <knowlesp2@cardiff.ac.uk>
2024-01-09 20:41:08 +0000

    improve config security, minor styling chages

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import os
+import secrets
 
 class Config:
-    SECRET_KEY = "pass"
+    SECRET_KEY = secrets.token_hex(16)
     SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(os.path.dirname(__file__), "database.db")}'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- server.py
+++ server.py
@@ -21,12 +21,7 @@ server.register_blueprint(blueprints, url_prefix = '/')
 
 with server.app_context():
     if not path.exists("database.db"):
-        print("Creating tables")
-        try:
             db.create_all()
-            print("Tables created successfully")
-        except Exception as e:
-            print(f"Error creating tables: {e}")
 
     profanity.load_censor_words()
     csrf = CSRFProtect(server)
