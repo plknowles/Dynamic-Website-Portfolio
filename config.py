cbd49efefffa07d5e76fac26170a35b72c3a43a2
Peter Knowles <knowlesp2@cardiff.ac.uk>
2024-01-10 06:50:32 +0000

    disable secret key generation to allow OpenShift Deployment

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 import secrets
 
 class Config:
-    # SECRET_KEY = secrets.token_hex(16)
-    SECRET_KEY = "test"
+    # SECRET_KEY = secrets.token_hex(16) # this wasn't allowing me to deploy to OpenShift for some reason
+    SECRET_KEY = "42db308d17d056e7bc1b095d6a0ba6c9"
     SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(os.path.dirname(__file__), "database.db")}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
