acd575e6387fd9e0611e85cb2f63fca834c25ea6
Peter Knowles <knowlesp2@cardiff.ac.uk>
2024-01-10 06:42:22 +0000

    troubleshooting deployment

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 import secrets
 
 class Config:
-    SECRET_KEY = secrets.token_hex(16)
+    # SECRET_KEY = secrets.token_hex(16)
+    SECRET_KEY = "test"
     SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(os.path.dirname(__file__), "database.db")}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
